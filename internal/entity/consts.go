e8984badd77937fb66deee7456ef0341b6465645
CaXaPCMeDoM <ZGHTLGHBYBVFNTKM@mail.ru>
2025-05-05 22:34:13 +0300

    refactor: const can be simplified

--- internal/entity/consts.go
+++ internal/entity/consts.go
@@ -5,19 +5,23 @@ const (
 	TimeFormat          = "15:04:05"
 
 	NumberShotsAtTheFiringLine = 5
+)
 
-	EventRegistered      = 1
-	EventStartTimeSet    = 2
-	EventOnStartLine     = 3
-	EventStarted         = 4
-	EventOnFiringRange   = 5
-	EventTargetHit       = 6
-	EventLeftFiringRange = 7
-	EventEnteredPenalty  = 8
-	EventLeftPenalty     = 9
-	EventEndedLap        = 10
-	EventCantContinue    = 11
-
+const (
 	EventDisqualified = 100
 	EventFinished     = 101
+)
+
+const (
+	EventRegistered = iota + 1
+	EventStartTimeSet
+	EventOnStartLine
+	EventStarted
+	EventOnFiringRange
+	EventTargetHit
+	EventLeftFiringRange
+	EventEnteredPenalty
+	EventLeftPenalty
+	EventEndedLap
+	EventCantContinue
 )
