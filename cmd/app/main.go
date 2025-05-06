df4a490be86b12cc92a1502dcbd7af168b52181e
CaXaPCMeDoM <ZGHTLGHBYBVFNTKM@mail.ru>
2025-05-06 21:44:35 +0300

    fix: delete default path for output from main

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -13,8 +13,8 @@ import (
 func main() {
 	configPath := flag.String("config", "./sunny_5_skiers/config.json", "Path to configuration file")
 	eventsPath := flag.String("events", "./sunny_5_skiers/events", "Path to events file")
-	outputLogPath := flag.String("output-log", "./result/output.log", "Path to output log file (optional)")
-	resultsPath := flag.String("results", "./result/results", "Path to results file (optional)")
+	outputLogPath := flag.String("output-log", "", "Path to output log file (optional)")
+	resultsPath := flag.String("results", "", "Path to results file (optional)")
 
 	flag.Parse()
 
